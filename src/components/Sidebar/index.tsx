f5f0cba23a6cf27e2f7a15c0417bb95437e1022f
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-04-24 09:07:14 -0300

    criando elemento de navegação

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,20 @@
 import { Container } from "./styles"
+import { ReactComponent as BurguerIcon } from '../../assets/burger.svg'
+import menuImg from '../../assets/menu.svg'
 export function Sidebar() {
-    return <Container>Sidebar</Container>
+    return <Container>
+        <button type="button">
+            <img src={menuImg} alt="Abrir e fechar o menu"/>
+        </button>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">
+                        <BurguerIcon />
+                        <span>Hambúrgueres</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </Container>
 }
