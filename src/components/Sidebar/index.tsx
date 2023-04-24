b0e5db89982d38ba90695e9e3758b98a22579b3e
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-04-24 09:29:36 -0300

    adicionando estilos

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,20 +1,22 @@
-import { Container } from "./styles"
+import { Container } from './styles'
 import { ReactComponent as BurguerIcon } from '../../assets/burger.svg'
 import menuImg from '../../assets/menu.svg'
 export function Sidebar() {
-    return <Container>
-        <button type="button">
-            <img src={menuImg} alt="Abrir e fechar o menu"/>
-        </button>
-        <nav>
-            <ul>
-                <li>
-                    <a href="#">
-                        <BurguerIcon />
-                        <span>Hambúrgueres</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
+  return (
+    <Container>
+      <button type='button'>
+        <img src={menuImg} alt='Abrir e fechar o menu' />
+      </button>
+      <nav>
+        <ul>
+          <li>
+            <a href='#' >
+              <BurguerIcon />
+              <span>Hambúrgueres</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </Container>
+  )
 }
--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -1,11 +1,73 @@
 import styled from "styled-components";
 
 export const Container = styled.aside`
-background-color: ${({theme}) => theme.colors.red};
+background-color: ${({ theme }) => theme.colors.red};
 width: 7.75rem;
 padding: 2rem 0;
 overflow: hidden;
 display: flex;
 flex-direction: column;
 align-items: center;
+
+button {
+    background: none;
+    width: 100%;
+    border: none;
+}
+
+nav {
+    flex: 1;
+    width: 100%;
+    height: 100%;
+
+    ul {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 1.5rem;
+    }
+    
+    li{
+        a{
+            width: fit-content;
+            position: relative;
+            padding-left: 1.875rem;
+            padding-right: 1.875rem;
+
+            display: flex;
+            align-items: center;
+            gap: 2rem;
+
+            svg {
+                fill: ${({ theme }) => theme.colors.white};
+                width: 4rem;
+                height: 4rem;
+                transition: fill 0.3s;
+            }
+
+            span {
+                font: 1rem;
+                font-weight: 500;
+                transition: color 0.3s;
+            }
+            &.active {
+                &::after {
+                    content: '';
+                    position: absolute;
+                    left: 0;
+                    top: 50%;
+                    bottom: 0;
+                    transform:  translateY(-50%);
+                    background-color: ${({ theme }) => theme.colors.yellow};
+                    width: 5px;
+                    height: calc(100% + 10px);
+                    border-radius: 0 5px 5px 0;
+                }
+            }
+        }
+    }
+}
+
+
 `
