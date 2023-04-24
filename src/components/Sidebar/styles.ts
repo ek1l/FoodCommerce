8714ff5eaee9eeec035c8928aea3a969a526e3c0
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-04-24 10:11:08 -0300

    adicionando responsividade na SideBar

--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -30,6 +30,8 @@ button {
     border: none;
 }
 
+
+
 nav {
     flex: 1;
     width: 100%;
@@ -83,6 +85,56 @@ nav {
         }
     }
 }
+
+@media (max-width: 720px) {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 999;
+
+    width: 100%;
+    height: 5rem;
+    overflow-y: auto;
+    padding: 0 0;
+
+    button {
+      display: none;
+    }
+
+    nav {
+      height: 100%;
+
+      ul {
+        flex-direction: row;
+        align-items: center;
+      }
+
+      li {
+        a {
+          flex-direction: column;
+          padding: 0rem;
+
+          svg {
+            width: 3.25rem;
+            height: 3.25rem;
+          }
+
+          span {
+            display: none;
+          }
+
+          &.active {
+            &::after {
+              display: none;
+            }
+          }
+        }
+      }
+    }
+  }
+
+
 
 
 `
