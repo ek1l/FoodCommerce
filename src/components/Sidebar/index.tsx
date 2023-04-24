16fe7c5867e49ab97fc194e4e3c4a873d5799c88
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-04-24 09:38:10 -0300

    adicionando mais icones na sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,8 @@
 import { Container } from './styles'
 import { ReactComponent as BurguerIcon } from '../../assets/burger.svg'
+import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg'
+import { ReactComponent as SodaPopicon } from '../../assets/soda.svg'
+import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg'
 import menuImg from '../../assets/menu.svg'
 export function Sidebar() {
   return (
@@ -10,9 +13,30 @@ export function Sidebar() {
       <nav>
         <ul>
           <li>
-            <a href='#' >
+            <a href='#' className='active'>
               <BurguerIcon />
               <span>Hambúrgueres</span>
+            </a>
+          </li>
+
+          <li>
+            <a href='#'>
+              <PizzaIcon />
+              <span>Pizza</span>
+            </a>
+          </li>
+
+          <li>
+            <a href='#'>
+              <SodaPopicon />
+              <span>Bebidas</span>
+            </a>
+          </li>
+
+          <li>
+            <a href='#'>
+              <IceCreamIcon />
+              <span>Sorvetes</span>
             </a>
           </li>
         </ul>
