f69a9df2b4bef32e7ba1375425b6b61c8b2a20b7
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-04-25 12:12:00 -0300

    Adicionando função de somar e atualizar preço no carrinho

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -20,8 +20,8 @@ interface RemoveSnackFromCart {
 interface CartContextProps {
   cart: Snack[]
   addSnackIntoCart: (snack: SnackData) => void
-//   removeSnackFromCart: ({ id, snack }: RemoveSnackFromCart) => void
-//   updateCart: ({ id, snack, newQuantity }: updateCartProps) => void
+  //   removeSnackFromCart: ({ id, snack }: RemoveSnackFromCart) => void
+  //   updateCart: ({ id, snack, newQuantity }: updateCartProps) => void
 }
 
 interface CartProviderProps {
@@ -30,19 +30,39 @@ interface CartProviderProps {
 export const CartContext = createContext({} as CartContextProps)
 
 export function CartProvider({ children }: CartProviderProps) {
-    const [cart, setCart] = useState<Snack[]>([])
+  const [cart, setCart] = useState<Snack[]>([])
 
-    function addSnackIntoCart(snack: SnackData): void {
-        const newSnack = { ...snack, quantity: 1, subtotal: snack.price }
-        const newCart = [...cart, newSnack]
-      console.log(`newCart`, newCart)
+  function addSnackIntoCart(snack: SnackData): void {
+    // buscar
 
-        setCart(newCart)
-    }
-
-    return (
-        <CartContext.Provider value={{cart,addSnackIntoCart}}>
-            {children}
-        </CartContext.Provider>
+    const snackExistentInCart = cart.find(
+      (item) => item.snack === snack.snack && item.id === snack.id,
     )
+
+    // atualizar
+    if (snackExistentInCart) {
+      const newCart = cart.map((item) => {
+        if (item.id === snack.id) {
+          const quantity = item.quantity + 1
+          const subtotal = item.price * quantity
+
+          return { ...item, quantity, subtotal }
+        }
+
+        return item
+      })
+      console.log(`newCart atualização`, newCart)
+      setCart(newCart)
+      return
+    }
+    // adicionar
+
+    const newSnack = { ...snack, quantity: 1, subtotal: snack.price }
+    const newCart = [...cart, newSnack]
+
+    console.log(`newCart adição`, newCart)
+    setCart(newCart)
+  }
+
+  return <CartContext.Provider value={{ cart, addSnackIntoCart }}>{children}</CartContext.Provider>
 }
