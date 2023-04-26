e77f5e4255bbb70a5aa0a45c1ce3a1845ec76495
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-04-26 07:32:35 -0300

    Adicionando estilos

--- src/components/OrderHeader/index.tsx
+++ src/components/OrderHeader/index.tsx
@@ -1,15 +1,25 @@
 import { Link } from 'react-router-dom'
+import { useCart } from '../../hooks/useCart'
 import logoImg from '../../assets/logo.svg'
 import { Container } from './styles'
+import { ReactComponent as CartIcon } from '../../assets/shopping-cart.svg'
 
 export function OrderHeader() {
+  const { cart } = useCart()
   return (
     <Container>
-        <Link to='/'>
+      <Link to='/'>
         <img src={logoImg} alt='Logo' />
-        </Link>
-
-     
+      </Link>
+      <div>
+        <div>
+          <h3>Meus pedidos</h3>
+          <span>
+            <strong>{`${cart.length}`.padStart(2, '0')} lanche(s)</strong>
+          </span>
+        </div>
+        <CartIcon />
+      </div>
     </Container>
   )
 }
--- src/components/OrderHeader/styles.ts
+++ src/components/OrderHeader/styles.ts
@@ -12,4 +12,39 @@ img{
     width: 8rem;
 }
 
+>div {
+background: none;
+border: none;
+display: flex;
+align-items: center;
+gap: 0.5rem;
+
+
+div {
+    text-align: right;
+    h3 {
+        margin-bottom: 0%.125rem;
+        font-weight: 500;
+        color: ${({theme}) => theme.colors.white};
+    }
+
+    span {
+        color: ${({theme}) => theme.colors.yellow};
+
+        strong {
+            font-weight: 500;
+            font-size: 1.25rem;
+        }
+    }
+
+}
+
+svg {
+    flex-shrink: 0;
+    fill: ${({theme}) => theme.colors.white};
+    width: 2.875rem;
+    height: 2.875rem;
+}
+}
+
 `
