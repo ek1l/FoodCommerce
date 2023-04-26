cba67b00ce81c7a1ad658503b6864fa5a354820e
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-04-26 10:24:54 -0300

    Alterando o design para quando o carrinho estiver vazio

--- src/pages/myCart/Table/index.tsx
+++ src/pages/myCart/Table/index.tsx
@@ -1,26 +1,27 @@
 import { useEffect, useState } from 'react'
 import { useCart } from '../../../hooks/useCart'
+
 import { TableDesktop } from './TableDesktop'
 import { TableMobile } from './TableMobile'
-export function Table() {
-  const [windowWidth, setWindowWidth] = useState(document.documentElement.clientWidth)
+import { EmptyCart } from '../../../components/EmptyCart'
 
+export function Table() {
   const { cart } = useCart()
+  const [windowWidth, setWindowWidth] = useState(document.documentElement.clientWidth)
 
   useEffect(() => {
     function updateTableComponentBasedInWindowWidth() {
       const currentWidth = document.documentElement.clientWidth
       setWindowWidth(currentWidth)
     }
-
     window.addEventListener('resize', updateTableComponentBasedInWindowWidth)
 
     return () => {
       window.removeEventListener('resize', updateTableComponentBasedInWindowWidth)
     }
-  },[])
+  }, [])
 
-  if (cart.length === 0){
-    return <h1>Ops! Parece que você não tem pedidos, peça já!</h1>
-  } return windowWidth > 768 ? <TableDesktop /> : <TableMobile />
+  if (cart.length === 0) return <EmptyCart  title=' Ops! Parece que você não tem pedidos, peça já! ' /> 
+
+  return windowWidth > 768 ? <TableDesktop /> : <TableMobile />
 }
