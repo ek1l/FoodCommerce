d0f81d418d26a2327a1f061bf3a8e5fce2eb41dc
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-04-26 09:22:28 -0300

    Ajustando carrinho para mobile

---
+++ src/pages/myCart/Table/TableMobile/index.tsx
@@ -0,0 +1,3 @@
+export function TableMobile() {
+    return<h1> Table Mobile</h1>
+}
--- src/pages/myCart/Table/index.tsx
+++ src/pages/myCart/Table/index.tsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react'
 import { useCart } from '../../../hooks/useCart'
 import { TableDesktop } from './TableDesktop'
+import { TableMobile } from './TableMobile'
 export function Table() {
+  const [windowWidth, setWindowWidth] = useState(document.documentElement.clientWidth)
+
   const { cart } = useCart()
+
+  useEffect(() => {
+    function updateTableComponentBasedInWindowWidth() {
+      const currentWidth = document.documentElement.clientWidth
+      setWindowWidth(currentWidth)
+    }
+
+    window.addEventListener('resize', updateTableComponentBasedInWindowWidth)
+
+    return () => {
+      window.removeEventListener('resize', updateTableComponentBasedInWindowWidth)
+    }
+  },[])
 
   if (cart.length === 0){
     return <h1>Ops! Parece que você não tem pedidos, peça já!</h1>
-  } return <TableDesktop />
+  } return windowWidth > 768 ? <TableDesktop /> : <TableMobile />
 }
