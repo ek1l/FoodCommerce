a458e202bb08310adb9c5c52b354af8d3054f78c
Leonardo Duarte <hintl3rgames2015@gmail.com>
2023-04-28 14:01:59 -0300

    Terminando o campo de preenchimento das informações

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,6 +1,7 @@
 import { Head } from '../../components/Head'
 import { OrderHeader } from '../../components/OrderHeader'
-import { Container, Inner, Form } from './styles'
+
+import { Container, Form, Inner } from './styles'
 
 export default function Payment() {
   return (
@@ -19,7 +20,7 @@ export default function Payment() {
           <div className='grouped'>
             <div className='field'>
               <label htmlFor='email'>E-mail</label>
-              <input type='text' name='email' id='email' autoComplete='email' />
+              <input type='email' name='email' id='email' autoComplete='email' />
             </div>
 
             <div className='field'>
@@ -28,13 +29,12 @@ export default function Payment() {
             </div>
 
             <div className='field'>
-              <label htmlFor='document'> CPF / CNPJ </label>
+              <label htmlFor='document'>CPF / CNPJ</label>
               <input type='text' id='document' name='document' />
             </div>
           </div>
 
           <h4>Endereço de entrega</h4>
-
 
           <div className='field'>
             <label htmlFor='zipcode'>CEP</label>
@@ -42,18 +42,117 @@ export default function Payment() {
               type='text'
               id='zipcode'
               name='zipcode'
-              autoComplete="'postal-code"
+              autoComplete='postal-code'
               style={{ width: '120px' }}
             />
           </div>
-          
+
           <div className='field'>
             <label htmlFor='street'>Endereço</label>
-            <input type='text' id='street' name='street' autoComplete='street-addess'/>
+            <input type='text' id='street' name='street' />
           </div>
 
-          <div className="field">Complemento</div>
-          <input type="text" id='complement' name='complement' /> 
+          <div className='grouped'>
+            <div className='field'>
+              <label htmlFor='number'>Número</label>
+              <input type='text' id='number' name='number' />
+            </div>
+
+            <div className='field'>
+              <label htmlFor='complement'>Complemento</label>
+              <input type='text' id='complement' name='complement' />
+            </div>
+          </div>
+
+          <div className='grouped'>
+            <div className='field'>
+              <label htmlFor='neighborhood'>Bairro</label>
+              <input type='text' id='neighborhood' name='neighborhood' />
+            </div>
+
+            <div className='field'>
+              <label htmlFor='city'>Cidade</label>
+              <input type='text' id='city' name='city' />
+            </div>
+
+            <div className='field'>
+              <label htmlFor='state'>Estado</label>
+              <select id='state' name='state'>
+                <option value=''>Selecione</option>
+                <option value='AC'>Acre</option>
+                <option value='AL'>Alagoas</option>
+                <option value='AP'>Amapá</option>
+                <option value='AM'>Amazonas</option>
+                <option value='BA'>Bahia</option>
+                <option value='CE'>Ceará</option>
+                <option value='ES'>Espírito Santo</option>
+                <option value='GO'>Goiás</option>
+                <option value='MA'>Maranhão</option>
+                <option value='MT'>Mato Grosso</option>
+                <option value='MS'>Mato Grosso do Sul</option>
+                <option value='MG'>Minas Gerais</option>
+                <option value='PA'>Pará</option>
+                <option value='PB'>Paraíba</option>
+                <option value='PR'>Paraná</option>
+                <option value='PE'>Pernambuco</option>
+                <option value='PI'>Piauí</option>
+                <option value='RJ'>Rio de Janeiro</option>
+                <option value='RN'>Rio Grande do Norte</option>
+                <option value='RS'>Rio Grande do Sul</option>
+                <option value='RO'>Rondônia</option>
+                <option value='RR'>Roraima</option>
+                <option value='SC'>Santa Catarina</option>
+                <option value='SP'>São Paulo</option>
+                <option value='SE'>Sergipe</option>
+                <option value='TO'>Tocantins</option>
+                <option value='DF'>Distrito Federal</option>
+              </select>
+            </div>
+          </div>
+
+          <h4>Pagamento</h4>
+
+          <div className='field'>
+            <label htmlFor='credit-card-number'>Número do cartão</label>
+            <input
+              type='text'
+              id='credit-card-number'
+              name='credit-card-number'
+              autoComplete='cc-number'
+            />
+          </div>
+
+          <div className='field'>
+            <label htmlFor='credit-card-holder-name'>Nome impresso no cartão</label>
+            <input
+              type='text'
+              id='credit-card-holder-name'
+              name='credit-card-holder-name'
+              autoComplete='cc-name'
+            />
+          </div>
+
+          <div className='grouped'>
+            <div className='field'>
+              <label htmlFor='credit-card-expiration'>Validade (MM/AA)</label>
+              <input
+                type='text'
+                id='credit-card-expiration'
+                name='credit-card-expiration'
+                autoComplete='cc-exp'
+              />
+            </div>
+
+            <div className='field'>
+              <label htmlFor='credit-card-code'>Código de segurança (CVV)</label>
+              <input
+                type='text'
+                id='credit-card-code'
+                name='credit-card-code'
+                autoComplete='cc-csc'
+              />
+            </div>
+          </div>
         </Form>
       </Inner>
     </Container>
--- src/pages/myCart/styles.ts
+++ src/pages/myCart/styles.ts
@@ -5,5 +5,4 @@ width: 100%;
 max-width: 58.75rem;
 padding: 2rem;
 margin: 0 auto;
-
 `
